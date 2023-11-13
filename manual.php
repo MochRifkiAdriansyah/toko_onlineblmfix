cb1bc868ef7b2e742a2087208ad1f24ddb65155f
MochRifkiAdriansyah <143516087+MochRifkiAdriansyah@users.noreply.github.com>
2023-11-13 21:42:20 +0700

    Update manual.php

--- manual.php
+++ manual.php
@@ -189,29 +189,6 @@ include 'header.php';
         </div>
     </section>
 </div>
-<?php
-// Get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
- 
-$to = "mochrifki98620@gmail.com";
-$subject = "This is the subject line";
- 
-
-$txt ="Name = ". $name . "\r\n  Email = "
-    . $email . "\r\n Message =" . $message;
- 
-$headers = "From: noreply@demosite.com" . "\r\n" .
-            "CC: somebodyelse@example.com";
-if($email != NULL) {
-    mail($to, $subject, $txt, $headers);
-}
- 
-// Redirect to
-header("Location:manual.php");
-?>
-
 
 <?php 
 include 'footer.php';
